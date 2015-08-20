dc74b2fa851643aaea6922909247e7ff73c7a9a8
Sydnee S. Charles <sydnee12sarah@yahoo.fr>
2015-08-20 15:09:48 -0700

    designe

--- app/src/main/java/com/example/photoviewer/photoviewer/TabsActivity.java
+++ app/src/main/java/com/example/photoviewer/photoviewer/TabsActivity.java
@@ -51,9 +51,9 @@ public class TabsActivity extends TabActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
